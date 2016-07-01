ba53571bae5d9c83b1db48223e2faa3f0a17e340
ghdbre <ghdbre@snafu.de>
2016-07-01 19:08:47 +0200

    Added list of watchlists for watchlist management activity

---
+++ app/src/main/java/de/dbremes/dbtradealert/WatchListManagementCursorAdapter.java
@@ -0,0 +1,57 @@
+package de.dbremes.dbtradealert;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import de.dbremes.dbtradealert.DbAccess.DbHelper;
+import de.dbremes.dbtradealert.DbAccess.WatchlistContract;
+
+public class WatchListManagementCursorAdapter extends CursorAdapter {
+    private Context context;
+    DbHelper dbHelper;
+
+    public WatchListManagementCursorAdapter(Context context, Cursor c, boolean autoRequery) {
+        super(context, c, autoRequery);
+        this.context = context;
+        this.dbHelper = new DbHelper(this.context);
+    } // ctor()
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        WatchListManagementDetailViewHolder holder
+                = (WatchListManagementDetailViewHolder) view.getTag();
+        holder.nameTextView.setText(cursor.getString(cursor
+                .getColumnIndex(WatchlistContract.Watchlist.NAME)));
+        holder.watchListId = cursor.getLong(cursor
+                .getColumnIndex(WatchlistContract.Watchlist.ID));
+    } // bindView()
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        View view = View.inflate(context, R.layout.layout_watchlists_management_detail, null);
+        // could replace WatchListManagementDetailViewHolder in ICS and above with
+        // view.setTag(R.id.my_view, myView);
+        WatchListManagementDetailViewHolder holder = new WatchListManagementDetailViewHolder();
+        holder.deleteButton = (Button) view.findViewById(R.id.deleteButton);
+        //holder.deleteButton.setOnClickListener(deleteButtonClickListener);
+        holder.editButton = (Button) view.findViewById(R.id.editButton);
+        //holder.editButton.setOnClickListener(editButtonClickListener);
+        holder.nameTextView = (TextView) view.findViewById(R.id.nameTextView);
+        holder.watchListId = cursor.getLong(cursor.getColumnIndex(WatchlistContract.Watchlist.ID));
+        view.setTag(holder);
+        return view;
+    } // newView()
+
+
+    private class WatchListManagementDetailViewHolder {
+        public Button deleteButton;
+        public Button editButton;
+        public TextView nameTextView;
+        public long watchListId;
+    } // class WatchListManagementDetailViewHolder
+} // class WatchListManagementCursorAdapter
--- app/src/main/java/de/dbremes/dbtradealert/WatchlistsManagementActivity.java
+++ app/src/main/java/de/dbremes/dbtradealert/WatchlistsManagementActivity.java
@@ -1,10 +1,18 @@
 package de.dbremes.dbtradealert;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import de.dbremes.dbtradealert.DbAccess.DbHelper;
 
 public class WatchlistsManagementActivity extends AppCompatActivity {
+    private Cursor cursor;
+    private DbHelper dbHelper;
+    private WatchListManagementCursorAdapter watchListManagementCursorAdapter;
 
     public void onCancelButtonClick(View view) {
         setResult(RESULT_CANCELED, getIntent());
@@ -15,6 +23,14 @@ public class WatchlistsManagementActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watchlists_management);
+        dbHelper = new DbHelper(this);
+        this.cursor = dbHelper.readAllWatchlists();
+        this.watchListManagementCursorAdapter
+                = new WatchListManagementCursorAdapter(this, this.cursor, false);
+        ListView watchListsListView = (ListView) findViewById(R.id.watchListsListView);
+        TextView emptyTextView = (TextView) findViewById(R.id.emptyTextView);
+        watchListsListView.setEmptyView(emptyTextView);
+        watchListsListView.setAdapter(watchListManagementCursorAdapter);
     } // ctor()
 
     public void onOkButtonClick(View view) {

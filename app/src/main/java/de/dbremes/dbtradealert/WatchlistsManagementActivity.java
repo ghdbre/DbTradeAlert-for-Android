f9de7a8cc015f513c14be10cbf7b0727751de744
ghdbre <ghdbre@snafu.de>
2016-07-05 16:23:11 +0200

    Added title to watchlist management screen

--- app/src/main/java/de/dbremes/dbtradealert/WatchlistsManagementActivity.java
+++ app/src/main/java/de/dbremes/dbtradealert/WatchlistsManagementActivity.java
@@ -24,6 +24,7 @@ public class WatchlistsManagementActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watchlists_management);
+        setTitle("Manage Watchlists");
         dbHelper = new DbHelper(this);
         this.cursor = dbHelper.readAllWatchlists();
         this.watchListManagementCursorAdapter

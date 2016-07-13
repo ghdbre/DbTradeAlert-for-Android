dccb3b8ad54198fc7d5a0495e2db4cd0ed8e67fb
ghdbre <ghdbre@snafu.de>
2016-07-13 18:27:40 +0200

    Implemented acting on auto_refresh_preference changes

--- app/src/main/java/de/dbremes/dbtradealert/QuoteRefreshScheduler.java
+++ app/src/main/java/de/dbremes/dbtradealert/QuoteRefreshScheduler.java
@@ -5,6 +5,7 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class QuoteRefreshScheduler extends BroadcastReceiver {
@@ -13,30 +14,37 @@ public class QuoteRefreshScheduler extends BroadcastReceiver {
     @Override
     @SuppressWarnings("NewApi")
     public void onReceive(Context context, Intent intent) {
-        // Create schedule for quote refresh
-        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         int requestCode = 0;
         Intent newIntent = new Intent(context, QuoteRefreshAlarmReceiver.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, requestCode,
                 newIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        if (Utils.isAndroidBeforeMarshmallow()) {
-            alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                    AlarmManager.INTERVAL_HOUR, AlarmManager.INTERVAL_HOUR, pendingIntent);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        boolean isAutoRefreshEnabled = PreferenceManager
+                .getDefaultSharedPreferences(context).getBoolean("auto_refresh_preference", false);
+        if (isAutoRefreshEnabled == false) {
+            alarmManager.cancel(pendingIntent);
+            Log.d(CLASS_NAME, "onReceive(): quote refresh cancelled");
         } else {
-            alarmManager.setAndAllowWhileIdle(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                    60 * 60 * 1000, pendingIntent);
-        }
-        // Use only for testing Doze and App Standby modes!
+            // Create schedule for quote refresh
+            if (Utils.isAndroidBeforeMarshmallow()) {
+                alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                        AlarmManager.INTERVAL_HOUR, AlarmManager.INTERVAL_HOUR, pendingIntent);
+            } else {
+                alarmManager.setAndAllowWhileIdle(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                        60 * 60 * 1000, pendingIntent);
+            }
+            // Use only for testing Doze and App Standby modes!
 //        alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
 //                1 * 60 * 1000, 1 * 60 * 1000, pendingIntent);
-        // Log what was done
-        String scheduleCreationType;
-        if (intent.getAction() != null
-                && intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
-            scheduleCreationType = "after reboot";
-        } else {
-            scheduleCreationType = "initially";
+            // Log what was done
+            String scheduleCreationType;
+            if (intent.getAction() != null
+                    && intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+                scheduleCreationType = "after reboot";
+            } else {
+                scheduleCreationType = "initially";
+            }
+            Log.d(CLASS_NAME, "onReceive(): quote refresh schedule created " + scheduleCreationType);
         }
-        Log.d(CLASS_NAME, "onReceive(): quote refresh schedule created " + scheduleCreationType);
     } // onReceive()
 } // class QuoteRefreshScheduler
--- app/src/main/java/de/dbremes/dbtradealert/SettingsActivity.java
+++ app/src/main/java/de/dbremes/dbtradealert/SettingsActivity.java
@@ -1,9 +1,17 @@
 package de.dbremes.dbtradealert;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
+    private static final String CLASS_NAME = "SettingsActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,4 +20,27 @@ public class SettingsActivity extends AppCompatActivity {
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
     } // onCreate()
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .unregisterOnSharedPreferenceChangeListener(this);
+    } // onPause()
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .registerOnSharedPreferenceChangeListener(this);
+    } // onResume()
+
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Log.d(CLASS_NAME, "onSharedPreferenceChanged: key = " + key);
+        if (key.equals("auto_refresh_preference")) {
+            Intent intent = new Intent(this, QuoteRefreshScheduler.class);
+            sendBroadcast(intent);
+        }
+    } // onSharedPreferenceChanged()
+
 } // class SettingsActivity

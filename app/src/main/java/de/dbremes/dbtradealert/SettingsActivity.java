b41f291b0f0de58b700ec42fd7a4be45fd1b9e4a
ghdbre <ghdbre@snafu.de>
2016-07-14 14:19:33 +0200

    Cleaned up

--- app/src/main/java/de/dbremes/dbtradealert/SettingsActivity.java
+++ app/src/main/java/de/dbremes/dbtradealert/SettingsActivity.java
@@ -1,10 +1,8 @@
 package de.dbremes.dbtradealert;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;

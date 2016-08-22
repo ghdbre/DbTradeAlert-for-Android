8ef2d7670f0ac5e9c0a705b30dd2b29e3c898184
ghdbre <ghdbre@snafu.de>
2016-08-22 16:33:58 +0200

    Added dummy AdHelper to playStore flavor

---
+++ app/src/playStore/java/de/dbremes/dbtradealert/AdHelper.java
@@ -0,0 +1,14 @@
+package de.dbremes.dbtradealert;
+
+import android.content.Context;
+import android.view.View;
+
+public class AdHelper {
+
+    public static View getAdView(Context context) {
+        return null;
+    } // getAdView()
+
+    public static void initialize(Context context) {
+    } // initialize()
+} // class AdHelper

3c3d7372d7bba5f3b542334bee6baa74e41c0e97
ghdbre <ghdbre@snafu.de>
2016-09-04 16:08:53 +0200

    Added showing actual ads for withAdsRelease variant; note that this needs the ad unit Id from project.rootDir/../../DbTradeAlert/project.properties

--- app/src/withAds/java/de/dbremes/dbtradealert/AdHelper.java
+++ app/src/withAds/java/de/dbremes/dbtradealert/AdHelper.java
@@ -9,13 +9,13 @@ import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 
 public class AdHelper {
-    private final static String SAMPLE_AD_UNIT_ID = "ca-app-pub-3940256099942544/6300978111";
 
     public static View getAdView(Context context) {
         // Create AdView
         AdView adView = new AdView(context);
         adView.setAdSize(AdSize.BANNER);
-        adView.setAdUnitId(SAMPLE_AD_UNIT_ID);
+
+        adView.setAdUnitId(BuildConfig.AD_UNIT_ID);
         // Load ad
         AdRequest adRequest = new AdRequest.Builder().build();
         adView.loadAd(adRequest);
@@ -23,6 +23,6 @@ public class AdHelper {
     } // getAdView()
 
     public static void initialize(Context context) {
-        MobileAds.initialize(context, SAMPLE_AD_UNIT_ID);
+        MobileAds.initialize(context, BuildConfig.AD_UNIT_ID);
     } // initialize()
 } // class AdHelper
